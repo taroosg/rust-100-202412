25e782b2bdaa974abc6ec38cfa3ff5590218a07d
taroosg <root@taroosg.dev>
2024-12-22 18:00:50 +0900

    done 10

--- src/exercises/exercise_10.rs
+++ src/exercises/exercise_10.rs
@@ -1,7 +1,12 @@
 /// 等差数列
 // 初項 m と公差 n を入力し、10番目までの数字をスペース区切りで出力する関数を作成してください。
 pub fn create_sequence(m: i32, n: i32) -> String {
-    todo!()
+    [m; 10]
+        .iter()
+        .enumerate()
+        .map(|(i, x)| (i as i32 * n + x).to_string())
+        .collect::<Vec<_>>()
+        .join(" ")
 }
 
 #[cfg(test)]
