5b7866f73df97c0ec7cb104de8b1a38a83b09e3f
taroosg <root@taroosg.dev>
2024-12-26 15:37:17 +0900

    done 37

--- src/exercises/exercise_37.rs
+++ src/exercises/exercise_37.rs
@@ -1,7 +1,10 @@
 /// 通知票
 // 数字の成績をアルファベットに変換する関数を作成してください。
 pub fn convert_number_to_alphabet(s: &str) -> String {
-    todo!()
+    s.chars()
+        .filter_map(|x| x.to_digit(10)) // 数値に変換
+        .map(|x| (x + 64) as u8 as char)
+        .collect()
 }
 
 #[cfg(test)]
