166baae8e33faaac08b1bef07a630a4b740f0a81
taroosg <root@taroosg.dev>
2024-12-26 15:33:27 +0900

    done 36

--- src/exercises/exercise_36.rs
+++ src/exercises/exercise_36.rs
@@ -1,7 +1,11 @@
 /// はじめまして
 // 名前と性別から挨拶を出力する関数を作成してください。
 pub fn greet(name: &str, gender: &str) -> String {
-    todo!()
+    match gender {
+        "M" => format!("Hi, Mr.{}", name),
+        "F" => format!("Hi, Ms.{}", name),
+        _ => format!("Hi, {}", name),
+    }
 }
 
 #[cfg(test)]
