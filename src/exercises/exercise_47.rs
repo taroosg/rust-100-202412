069dc3c837eacb8003881f3329c4d22864d88279
taroosg <root@taroosg.dev>
2024-12-27 14:05:48 +0900

    done 47

--- src/exercises/exercise_47.rs
+++ src/exercises/exercise_47.rs
@@ -1,7 +1,7 @@
 /// ガチャ期待値計算
 // 当選確率と試行回数から、1回以上当選する確率を計算する関数を作成してください。
 pub fn probability_get_ssr(n: f64, t: u32) -> u32 {
-    todo!()
+    100 - ((1.0 - n / 100.0).powi(t as i32) * 100.0).round() as u32
 }
 
 #[cfg(test)]
